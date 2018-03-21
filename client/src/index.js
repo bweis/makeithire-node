9db79de867657b94a92cec5dcb748bb471ab7878
Benjamin Weis <bweis96@gmail.com>
2018-03-21 01:29:43 -0400

    Begin Cleanup

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+// containers
+import Full from './containers/Full.js'
+// import 'bootstrap/dist/css/bootstrap.css';
+// import styles from './index.css'
+
+ReactDOM.render((
+
+    <Full/>
+), document.getElementById('root'));
