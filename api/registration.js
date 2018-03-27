83d8259da8565a15fb4ac7d80c5e9500e8672e08
Benjamin Weis <bweis96@gmail.com>
2018-03-26 21:22:09 -0700

    moved rajat's email stuff over

--- api/registration.js
+++ api/registration.js
@@ -1,5 +1,8 @@
+const { validateEmail } = require('./utils/emailValidation');
+
 const bcrypt = require('bcrypt');
 const db = require('./utils/db');
+
 
 function signUpStudent(req, res) {
   console.log(req.body);
@@ -9,22 +12,26 @@ function signUpStudent(req, res) {
   if (email === undefined || pass === undefined || email === '' || pass === '') {
     return res.status(400).json({ error: 'Email ID or Password cannot be empty' });
   }
+  if (!validateEmail(email)) {
+    return res.status(400).json({ error: 'Bad Email Format' });
+  }
+  if (!email.includes('.edu')) {
+    return res.status(400).json({ error: 'Email not a Student Email. Email should include use .edu' });
+  }
   bcrypt.hash(req.body.Password, Number(process.env.ROUNDS), (err, hash) => {
     const post = {
       FirstName: fname, MiddleName: (mname === undefined) ? mname : '', LastName: lname, EmailID: email, Password: hash, idCompany: 0,
     };
     const sql = 'INSERT INTO User SET ?';
-    db.query(sql, post, (db_err1, result) => {
-      if (db_err1) {
-        return res.status(400).json({ message: db_err1 });
+    db.query(sql, post, (err1, res1) => {
+      if (err1) {
+        return res.status(400).json({ message: err1 });
       }
-
-      sql2 = `INSERT INTO Student VALUES ('${result.insertId}', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)`;
-      db.query(sql2, (db_err2, result2) => {
-        if (db_err2) {
-          return res.status(400).json({ error: db_err2 });
+      const sql2 = `INSERT INTO Student VALUES ('${res1.insertId}', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)`;
+      db.query(sql2, (err2, res2) => {
+        if (err2) {
+          return res.status(400).json({ error: err2 });
         }
-
         return res.status(200).json({ message: 'Account created' });
       });
     });
@@ -56,36 +63,36 @@ function signUpRecruiter(req, res) {
     };
     const sql1 = 'INSERT INTO User SET ?';
     if (idComp == '-1') {
-      db.query(sql1, post, (db_err1, result1) => {
-        if (db_err1) {
-          return res.sendStatus(400).json({ error: db_err1 });
+      db.query(sql1, post, (err1, res1) => {
+        if (err1) {
+          return res.sendStatus(400).json({ error: err1 });
         }
         if (companyname == null) {
           post = {
-            CompanyName: null, Description: null, Logo: null, HashTags: null, idUser: result1.insertId, Published: 0, EmailDomain: emailDomain[1],
+            CompanyName: null, Description: null, Logo: null, HashTags: null, idUser: res1.insertId, Published: 0, EmailDomain: emailDomain[1],
           };
         } else if (published == '1') {
           post = {
-            CompanyName: companyname, Description: description, Logo: null, HashTags: null, idUser: result1.insertId, Published: published, EmailDomain: emailDomain[1],
+            CompanyName: companyname, Description: description, Logo: null, HashTags: null, idUser: res1.insertId, Published: published, EmailDomain: emailDomain[1],
           };
         } else {
           post = {
-            CompanyName: companyname, Description: description, Logo: null, HashTags: null, idUser: result1.insertId, Published: published, EmailDomain: emailDomain[1],
+            CompanyName: companyname, Description: description, Logo: null, HashTags: null, idUser: res1.insertId, Published: published, EmailDomain: emailDomain[1],
           };
         }
         const sql2 = 'INSERT INTO Company SET ?';
-        db.query(sql2, post, (db_err2, result2) => {
-          if (db_err2) {
-            return res.status(400).json({ error: db_err2 });
+        db.query(sql2, post, (err2, res2) => {
+          if (err2) {
+            return res.status(400).json({ error: err2 });
           }
 
-          const sql3 = `UPDATE User SET idCompany = '${result2.insertId}' WHERE idUser = '${result1.insertId}'`;
-          db.query(sql3, (db_err3, result3) => {
-            if (db_err3) {
-              return res.status(400).json({ error: db_err3 });
+          const sql3 = `UPDATE User SET idCompany = '${res2.insertId}' WHERE idUser = '${res1.insertId}'`;
+          db.query(sql3, (err3, res3) => {
+            if (err3) {
+              return res.status(400).json({ error: err3 });
             }
 
-            return res.status(200).json({ message: 'Account Created', response: result3 });
+            return res.status(200).json({ message: 'Account Created', response: res3 });
           });
         });
       });
