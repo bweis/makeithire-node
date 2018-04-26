8bee378daa52e6b22a6affc41e1139fc543e00eb
Rajat Srivastava <rajatsrivastava95@gmail.com>
2018-04-26 14:35:39 -0400

    bug fix

--- api/application.js
+++ api/application.js
@@ -2,7 +2,7 @@ const db = require('./utils/db');
 
 // Get Applicants for a Job
 function getApplicants(req, res) {
-  const sql = 'SELECT idUser, FirstName, MiddleName, LastName, SubmissionDate, SupplementaryAs FROM Application NATURAL JOIN User WHERE idJob = ?';
+  const sql = 'SELECT idUser, FirstName, MiddleName, LastName, SubmissionDate, SupplementaryAs FROM Application NATURAL JOIN User WHERE idJobs = ?';
   db.query(sql, req.body.idJob, (err, result) => {
     if (err) {
       return res.status(400)
